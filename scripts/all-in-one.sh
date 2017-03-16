7c25fcb87b401cd0d21956b19d16d3af104bb7f9
Juan Badia Payno <jbadiapa@redhat.com>
2017-03-16 11:01:46 +0100

    Added certificates scripts

--- scripts/all-in-one.sh
+++ scripts/all-in-one.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 vagrant up all  --provider virtualbox
-#vagrant up redis --provider virtualbox
-#vagrant up sensu --provider virtualbox
-#vagrant up uchiwa --provider virtualbox
-#vagrant up fluent --provider virtualbox
-#vagrant up elastic --provider virtualbox
-#vagrant up kibana --provider virtualbox
-#vagrant up grafana --provider virtualbox
-#vagrant up collectd --provider virtualbox
-
 # Git clone if the directory does not exits
 if [ ! opstools-ansible ]; then 
  git clone https://github.com/centos-opstools/opstools-ansible.git
 fi
+
+#Create Certs
+./scripts/cert-testca.sh
+./scripts/cert-server.sh
+./scripts/cert-client.sh
+
+#
 
 cd opstools-ansible
 
@@ -36,7 +34,6 @@ cat >hosts<<EOF
 [targets]
 192.168.33.51 ansible_user=vagrant ansible_ssh_extra_args='-i ../.vagrant/machines/all/virtualbox/private_key -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no' ansible_become=True ansible_become_user=root ansible_become_method=sudo
 EOF
-
 
 cd ..
 pwd
---
+++ scripts/cert-client.sh
@@ -0,0 +1,8 @@
+mkdir client
+cd client
+openssl genrsa -out key.pem 2048
+openssl req -new -key key.pem -out req.pem -outform PEM -subj /CN=$(hostname)/O=client/ -nodes
+cd ../testca
+openssl ca -config openssl.cnf -in ../client/req.pem -out ../client/cert.pem -notext -batch -extensions client_ca_extensions
+cd ../client
+openssl pkcs12 -export -out keycert.p12 -in cert.pem -inkey key.pem -passout pass:MySecretPassword
