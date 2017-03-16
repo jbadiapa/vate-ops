04b06d9838a4cd72bf601d700fa2055855cc2469
Juan Badia Payno <jbadiapa@redhat.com>
2017-03-16 12:25:34 +0100

    include the certs files on the installation start

--- scripts/all-in-one.sh
+++ scripts/all-in-one.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
-vagrant up all  --provider virtualbox
+#vagrant up all  --provider virtualbox
+
+####
 # Git clone if the directory does not exits
+##
 if [ ! opstools-ansible ]; then 
  git clone https://github.com/centos-opstools/opstools-ansible.git
 fi
 
+####
 #Create Certs
+##
 ./scripts/cert-testca.sh
 ./scripts/cert-server.sh
 ./scripts/cert-client.sh
-
 #
 
 cd opstools-ansible
 
+#####
 # Get the review if it is given  
-if [ #$ == 1];
+##
+if [ $# = 1 ]; then
  REVIEW = $1
  git review -d $REVIEW
 else
- fit checkout master
+ git checkout master
 fi
 
+#####
+# Creates the inventoy file
+##
 cd inventory
 cat >hosts<<EOF
 [am_hosts]
@@ -34,7 +43,41 @@ cat >hosts<<EOF
 [targets]
 192.168.33.51 ansible_user=vagrant ansible_ssh_extra_args='-i ../.vagrant/machines/all/virtualbox/private_key -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no' ansible_become=True ansible_become_user=root ansible_become_method=sudo
 EOF
-
 cd ..
-pwd
+
+####
+# Generate params.yml
+###
+
+function start_with(){
+	FILETOCHANGE=$1
+	TEXT=$2
+	sed -i -e "s/^/$TEXT/g" $FILETOCHANGE
+}
+
+function set_ssl_var(){
+	FILE=$1
+	VAR=$2
+	SSL_FILE=$3
+	LINES=`wc -l $SSL_FILE | awk '{print $1}'`
+	if [ $LINES -gt 1 ]; then
+	   start_with $SSL_FILE "  "
+           cat >>$FILE<<EOF
+$VAR: |
+EOF
+           cat $SSL_FILE >> $FILE
+	   cat >>$FILE<<EOF
+
+EOF
+        fi
+}
+cat >params.yml<<EOF
+---
+
+rabbitmq_use_ssl: true
+EOF
+set_ssl_var params.yml rabbitmq_ssl_cacert ../testca/cacert.pem
+set_ssl_var params.yml rabbitmq_ssl_cert ../server/cert.pem 
+set_ssl_var params.yml rabbitmq_ssl_key ../server/key.pem
+
 #ansible-playbook -i inventory playbook.yml
